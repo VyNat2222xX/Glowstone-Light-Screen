57474bcfb792b50b354e714d51e3f0d814706ed2
VyNat2222xX <ngoncalvesredon@edenschool.fr>
2026-03-17 17:08:42 +0100

    new files...

--- app.js
+++ app.js
@@ -1,35 +1,6 @@
-const $btnswitch = document.querySelector("#light");
-const $dialog = document.querySelector("#menu");
-let $stat = document.querySelector("#menu-code-status");
-const $span = document.querySelector("#menu-span-status");
-let $img = document.querySelector("#glowstone");
+const $dialogmenu = document.querySelector("#menu");
+const $btnlight = document.querySelector("light");
 
-let isOn = false;
-
-$btnswitch.addEventListener("click", () => {
-	$dialog.showModal();
-});
-
-$dialog.addEventListener("submit", () => {
-	if (isOn === false) {
-		$stat.textContent = "OFF";
-		$span.textContent = "OFF";
-
-		document.body.classList.remove("light-off");
-		document.body.classList.add("light-on");
-
-		$img.src = "assets/glowstone-on.webp";
-
-		isOn = true;
-	} else if (isOn === true) {
-		$stat.textContent = "ON";
-		$span.textContent = "ON";
-
-		document.body.classList.remove("light-on");
-		document.body.classList.add("light-off");
-
-		$img.src = "assets/glowstone-off.webp";
-
-		isOn = false;
-	}
+$btnlight.addEventListener("click", () => {
+	$dialogmenu.showModal();
 });

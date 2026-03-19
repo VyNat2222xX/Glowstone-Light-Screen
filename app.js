6887549087e306c68ad1b40490e9d0de0be4f8f9
VyNat2222xX <ngoncalvesredon@edenschool.fr>
2026-03-19 09:40:46 +0100

    removed everything

--- app.js
+++
@@ -1,6 +0,0 @@
-const $dialogmenu = document.querySelector("#menu");
-const $btnlight = document.querySelector("light");
-
-$btnlight.addEventListener("click", () => {
-	$dialogmenu.showModal();
-});
